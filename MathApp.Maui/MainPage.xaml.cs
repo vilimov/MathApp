6ef729604640aa990ddcfe5210e0de3842cef609
Милко Томов <el_loco@abv.bg>
2023-12-21 08:57:49 +0200

    Grid applied to MainPage

--- MathApp.Maui/MainPage.xaml.cs
+++ MathApp.Maui/MainPage.xaml.cs
@@ -4,7 +4,7 @@ namespace MathApp.Maui;
 
 public partial class MainPage : ContentPage
 {
-    readonly int count = 0;
+    //readonly int count = 0;
 
     public MainPage()
     {
@@ -26,41 +26,6 @@ public partial class MainPage : ContentPage
         SemanticScreenReader.Announce(CounterBtn.Text);*/
     }
 
-/*    private void CalculateNumber_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
-    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        string enteredNumber = ((Entry)sender).Text;
-
-        try
-        {
-            int number = int.Parse(enteredNumber);
-        }
-        catch (Exception ex)
-        {
-            DisplayErrorMessage("Invalid input! Please enter a valid integer!", sender);
-        }
-    }
-
-    private void DisplayErrorMessage(string message, object sender)
-    {
-        Label errorLabel = new Label();
-        errorLabel.Text = message;
-        errorLabel.FontSize = 14;
-        errorLabel.TextColor = Color.FromRgb(255, 0, 0);
-
-        errorLabel.VerticalOptions = LayoutOptions.Center;
-        errorLabel.HorizontalOptions = LayoutOptions.Center;
-
-        ContentPage page = ((Entry)sender).Parent.Parent as ContentPage;
-
-        page.Content = errorLabel;
-
-        errorLabel.FadeTo(0, 2000, Easing.CubicOut);
-    }*/
 
     private void PrimeNumber_Clicked(object sender, EventArgs e)
     {
@@ -82,63 +47,14 @@ public partial class MainPage : ContentPage
 
     }
 
-    /*private void HandleButtonClicked(object sender, EventArgs e)
+    private void About_Clicked(object sender, EventArgs e)
     {
-        // Create a popup window
-        Popup popup = new Popup();
-        popup.VerticalOptions = LayoutOptions.Center;
-        popup.HorizontalOptions = LayoutOptions.Center;
 
-        // Create an Entry field for user input
-        Entry entry = new Entry();
-        entry.Placeholder = "Input a number";
-        entry.HorizontalOptions = LayoutOptions.Center;
+    }
 
-        // Add the Entry field to the popup
-        popup.Content = entry;
+    private void TestButton_Clicked(object sender, EventArgs e)
+    {
+        this.ShowPopup(new PopupPage());
 
-        // Open the popup window
-        popup.IsOpen = true;
-
-        // Handle the popup's closed event
-        popup.Closed += (sender, args) =>
-        {
-            // Get the entered number from the Entry field
-            string enteredNumber = ((Entry)sender).Text;
-
-            // If the entered number is not empty, perform calculations
-            if (!string.IsNullOrEmpty(enteredNumber))
-            {
-                // Perform the appropriate calculations based on the button clicked
-                if (sender == PrimeNumber)
-                {
-                    // Check if the entered number is a prime number
-                    if (IsPrime(int.Parse(enteredNumber)))
-                    {
-                        ShowMessage("The number is prime.");
-                    }
-                    else
-                    {
-                        ShowMessage("The number is not prime.");
-                    }
-                }
-                else if (sender == FactorialNumber)
-                {
-                    // Calculate the factorial of the entered number
-                    int factorial = CalculateFactorial(int.Parse(enteredNumber));
-                    ShowMessage($"The factorial of the number is {factorial}.");
-                }
-                else if (sender == NOK)
-                {
-                    // Prompt the user to enter another number for NOK calculation
-                    ShowMessage("Enter another number for NOK calculation:");
-                }
-                else if (sender == NOD)
-                {
-                    // Prompt the user to enter another number for NOD calculation
-                    ShowMessage("Enter another number for NOD calculation:");
-                }
-            }
-        };
-    }*/
+    }
 }
